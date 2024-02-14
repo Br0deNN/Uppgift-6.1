2a33334e148ebd6534d2beecd461eee2151c5e44
7R1XY <theo.broden@gmail.com>
2024-02-14 10:34:02 +0100

    Add project files.

---
+++ Uppgift 6.1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Skriv in tre tal");
+        Console.WriteLine("Skriv in tal 1");
+        int tal1 = int.Parse(Console.ReadLine());
+        Console.WriteLine("Skriv in tal 2");
+        int tal2 = int.Parse(Console.ReadLine());
+        Console.WriteLine("Skriv in tal 3");
+        int tal3 = int.Parse(Console.ReadLine());
+        
+        Console.WriteLine($"Summan av tallen blir {Addera(tal1, tal2, tal3)}");
+
+    }
+
+    static int Addera(int tal1, int tal2 , int tal3)
+    {
+        return tal1 + tal2 + tal3;
+       
+    }
+}
